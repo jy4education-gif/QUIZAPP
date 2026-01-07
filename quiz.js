0b3501902410fd40e59b4b048767bb80d64b9bbe
jy4education-gif <jy4education@gmail.com>
2026-01-07 11:52:40 +0100

    first commit

---
+++ quiz.js
@@ -0,0 +1,36 @@
+let score = 0;  // wird in JS automatisch erkannt ob float oder integer - naming convention variable bezeichnend und klein geschrieben
+let currentQuestionIndex = 0; // wir arbeiten später mit arrays - Screibweise hier CamelCase
+
+let questions = [
+    { 
+        question: "Was ist die Haupstadt von Berlin?", // "," Verknüpfung mehrerer Eigenschaften
+        answers : ["Paris", "London", "Berlin", "Madrid"], // "[" Array 
+        correctAnswer: 2 // "2" Position im Array 
+    },
+    { 
+        question: "Welche Farbe hat eine Zitrone?", 
+        answers : ["Gelb", "Blau", "Grün", "Rot"], 
+        correctAnswer: 0
+    },
+    { 
+        question: "Wieviele Beine hat ein Hund?", 
+        answers : ["2", "6", "8", "4"], 
+        correctAnswer: 3
+    }
+];
+
+function displayQuestion(){
+    let question = questions[currentQuestionIndex];
+
+    document.getElementById('question').textContent = question.question;
+
+    let answersDiv = document.getElementById('answers');
+
+    question.answers.forEach((answer, index) => {
+        let answerButton = document.createElement('button');
+        answerButton.textContent = answer;
+        answersDiv.appendChild(answerButton);
+    });
+} 
+
+displayQuestion();
